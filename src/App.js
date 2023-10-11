c6aa6484fdc430cf79b791ef84b12bf1f4e56236
Affan <affanattar12345@gmail.com>
2023-10-11 15:55:09 +0000

    change player

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
-import './App.css';
+import "./index.css";
+import Song from "./Song";
+import { React, useState } from "react";
 
 function App() {
+  const music_player_label = document.querySelector("#music-player-label");
+  const music_input = document.querySelector("#music-input");
+  const music_player_output = document.querySelector("#music-player-output");
+  const songname = document.querySelector("#songname");
+  const audio = document.querySelector("#audio");
+
+  const [audioUrl, setAudioUrl] = useState("");
+
+  function handlefileinput(e) {
+    if (!e.target.files.length) return;
+
+    setAudioUrl(URL.createObjectURL(e.target.files[0]));
+    audio.addEventListener("load", () => {
+      URL.revokeObjectURL(audioUrl);
+    });
+    audio.src = audioUrl;
+    audio.controls = "true";
+    // console.log(e.target.files[0].name)
+    songname.innerText = `${e.target.files[0].name}`;
+  }
+
+  function handleDragover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    // Style the drag-and-drop as a "copy file" operation.
+    e.dataTransfer.dropEffect = "copy";
+  }
+
+  function handleDrop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  
+    const fileList = [e.dataTransfer.files];
+  
+    console.log(fileList);
+
+    setAudioUrl(URL.createObjectURL(fileList[0]))
+  
+    // audio.addEventListener("load", () => {
+    //   URL.revokeObjectURL(audioUrl);
+    // });
+  
+    audio.src = audioUrl;
+    audio.controls = "true";
+    songname.innerText = `${fileList[0].name}`;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <div id="music-player">
+        <label
+          id="music-player-label"
+          htmlFor="music-input"
+          onDragOver={handleDragover}
+          onDrop={handleDrop}
+        >
+          <span>Drag and drop or select a song to play</span>
+        </label>
+        <input
+          onChange={handlefileinput}
+          hidden
+          id="music-input"
+          type="file"
+          name="music-input"
+          accept=".mp3"
+        />
+        <div id="music-player-output">
+          {() => {
+            return <Song />;
+          }}
+        </div>
+      </div>
+    </>
   );
 }
 
